02e3df2017b64f70999223bf933b4e2848133c08
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2016-10-30 20:33:07 +0300

    grub2-s390x-03-output-7-bit-ascii

--- grub-core/kern/emu/main.c
+++ grub-core/kern/emu/main.c
@@ -189,6 +189,12 @@ static struct argp argp = {
   NULL, help_filter, NULL
 };
 
+void
+ignore (int num __attribute__ ((unused)))
+{
+  return;
+}
+
 
 
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"
@@ -258,7 +264,7 @@ main (int argc, char *argv[])
       sleep (1);
     }
 
-  signal (SIGINT, SIG_IGN);
+  signal (SIGINT, (sighandler_t) &ignore);
   grub_console_init ();
   grub_host_init ();
 
